1c7554ff68784c87e78b6821131fa363c68c45a9
Hegaja <hegaja@gmail.com>
2018-08-17 16:43:43 -0300

    some clean at the code, removed some unused commented code

--- script.js
+++ script.js
@@ -1,25 +1,17 @@
 input = 16;
 grid = Math.pow(input, 2);
-// let initiateGrid = 0;
 
+for (let j = 1; j <= grid; j++) {
+  let div = document.createElement("div");
+  div.classList.add("cell");
+  div.id = "cell";
+  div.style.width = 100 / input + "%";
+  div.style.height = 100 / input + "%";
 
-// if (initiateGrid == 0) {
-//   initiateGrid = 1;
-//   createTable(grid);
-// }
+  div.style.background = "white";
 
-
-  for (let j = 1; j <= grid; j++) {
-    let div = document.createElement("div");
-    div.classList.add("cell");
-    div.id = "cell";
-    div.style.width = 100 / input + "%";
-    div.style.height = 100 / input + "%";
-
-    div.style.background = "white";
-
-    document.getElementById("gridArena").appendChild(div);
-  }
+  document.getElementById("gridArena").appendChild(div);
+}
 
 const cells = document.querySelectorAll(".cell");
 cells.forEach(div => {
@@ -29,7 +21,6 @@ cells.forEach(div => {
 });
 
 function buttonReset() {
-
   for (var j = 1; j <= grid; j++) {
     const container = document.querySelector(`#gridArena`);
     const cells = document.querySelector(`.cell`);
@@ -39,10 +30,6 @@ function buttonReset() {
   let gridSize = prompt("Please enter the size of the new grid", "16");
   gridSize = parseInt(gridSize);
   grid = Math.pow(gridSize, 2);
-
-  
-  // console.log(gridSize);
-  // createTable(grid);
 
   for (let j = 1; j <= grid; j++) {
     let div = document.createElement("div");
